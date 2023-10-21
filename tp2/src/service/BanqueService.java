eeb38ea15c1422a3e2c9b0be75f3fc190c47d02a
unknown <taktouknada76@gmail.com>
2023-10-21 12:42:25 +0100

    tp2_soa

--- tp2/src/service/BanqueService.java
+++ tp2/src/service/BanqueService.java
@@ -1,9 +1,36 @@
 package service;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
 	public class BanqueService {
 	public double conversion(double mt) {
 	return mt*10.6;
 	}
+	   private Compte compte;
+	    private List<Compte> comptes;
+
+	    public Compte getCompte() {                                 
+	        return new Compte(12345, 1000.0, "2023-10-21"); // Exemple, remplacez par la logique réelle
+	   
+	    }
+
+	    public void setCompte(Compte compte) {
+	        this.compte = compte;
+	    }
+
+	    public List<Compte> getComptes() {
+	        List<Compte> comptes = new ArrayList<Compte>();
+	        comptes.add(new Compte(12345, 1000.0, "2023-10-21")); // Exemple, remplacez par la logique réelle
+	        comptes.add(new Compte(67890, 2000.0, "2023-10-22")); // Exemple, remplacez par la logique réelle
+	       
+	        return comptes;
+	    }
+
+	    public void setComptes(List<Compte> comptes) {
+	        this.comptes = comptes;
+	    }
+	    
 
 	
 	
--- tp2/src/service/Main.java
+++ tp2/src/service/Main.java
@@ -1,13 +1,29 @@
 package service;
 
+import java.util.List;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		BanqueService bs = new BanqueService();
+		BanqueService banqueService = new BanqueService();
 		double montantEnUCD=100.0;
-		double montantConverti=bs.conversion(montantEnUCD);
+		double montantConverti= banqueService.conversion(montantEnUCD);
 		System.out.println("montant converti:"+montantConverti);
-		
-		}
+	 Compte compte = banqueService.getCompte();
+	        List<Compte> comptes = banqueService.getComptes();
+	       
+		 
+	                for (Compte C : comptes) {
+	                    System.out.println("Code: " + C.getCode());
+	                    System.out.println("Solde: " + C.getSolde());
+	                    System.out.println("Date de création: " + C.getDateCreation());
+	                }
+	          }
+	   }	        
 
-}
+
+	    
+
+	
+
