b983fb7e6587e4d1b9a9cde79fcd972f2ea3fd5c
unknown <taktouknada76@gmail.com>
2023-10-24 13:10:34 +0100

    tp2_devpmobile

---
+++ taktouknada_tp2_devppmobile/app/src/main/java/com/example/taktouknada_tp2_devppmobile/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.taktouknada_tp2_devppmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public
+}
